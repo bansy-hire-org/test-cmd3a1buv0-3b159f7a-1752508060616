8b09c59310b4a21f12c1a44430566d46d323d12b
Bansy Assessment Platform <noreply@bansy.com>
2025-07-14 15:47:44 +0000

    Add src/components/TaskItem.vue

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <li>
+    <span @click="toggleComplete">{{ task.name }}</span>
+    <button @click="deleteTask">Delete</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    task: {
+      type: Object,
+      required: true
+    },
+    index: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    toggleComplete() {
+      this.$emit('toggle-complete', this.index);
+    },
+    deleteTask() {
+      this.$emit('delete-task', this.index);
+    }
+  }
+}
+</script>
